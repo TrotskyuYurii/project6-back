465d9b90858651df119ebb7d92d890739eee84e7
VadimTrubay <vadnetvadnet@ukr.net>
2024-07-12 13:35:29 +0300

    Update water.js

--- src/schemas/water.js
+++ src/schemas/water.js
@@ -1,17 +1,17 @@
 import Joi from 'joi';
 
 export const addWaterSchema = Joi.object({
-  date: Joi.date().iso().required(), // Validate date in ISO 8601 format (YYYY-MM-DD)
+  date: Joi.date().iso().required(),
   amount: Joi.number().min(50).max(1500).required(),
   time: Joi.string()
-    .regex(/^\d{2}:\d{2}$/) // Validate time in HH:mm format
+    .regex(/^\d{2}:\d{2}$/)
     .required(),
 });
 
 export const editWaterSchema = Joi.object({
-  date: Joi.date().iso().required(), // Validate date in ISO 8601 format (YYYY-MM-DD)
+  date: Joi.date().iso().required(),
   amount: Joi.number().min(50).max(1500).required(),
   time: Joi.string()
-    .regex(/^\d{2}:\d{2}$/) // Validate time in HH:mm format
+    .regex(/^\d{2}:\d{2}$/)
     .required(),
 });
