795925f0a40340cc998b659e94b36835978132f5
VadimTrubay <vadnetvadnet@ukr.net>
2024-07-14 21:26:22 +0300

    refact

--- src/services/water.js
+++ src/services/water.js
@@ -1,26 +1,43 @@
 import { WatersCollection } from '../db/models/waterModel.js';
 
 
-  export const addWater = async (payload) => {
-    return WatersCollection.create(payload);
+export const addWater = async (payload) => {
+  return WatersCollection.create(payload);
+};
+
+export const editWater = async (waterId, payload, options = {}) => {
+  const rawResult = await WatersCollection.findOneAndUpdate(
+    waterId,
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    water: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
   };
+};
 
-  export const editWater = async (waterId, payload) => {
-    return WatersCollection.findByIdAndUpdate(waterId, payload, { new: true });
-  };
+export const deleteWater = async (waterId) => {
+  const water = await WatersCollection.findOneAndDelete(waterId);
 
-  export const deleteWater = async (waterId) => {
-    return WatersCollection.findByIdAndDelete(waterId);
-  };
+  return water;
+};
 
-  export const getDayWater = async (date, payload) => {
-    return WatersCollection.find({ date: date });
-  };
+export const getDayWater = async (date, payload) => {
+  return WatersCollection.find({ date: date });
+};
 
-  export const getMonthWater = async (date, payload) => {
+export const getMonthWater = async (date, payload) => {
 
-    const [_, month, year] = date.split('-');
-    const dateRegex = new RegExp(`^\\d{2}-${month}-${year}$`);
+  const [_, month, year] = date.split('-');
+  const dateRegex = new RegExp(`^\\d{2}-${month}-${year}$`);
 
-    return WatersCollection.find({ date: dateRegex });
-  };
+  return WatersCollection.find({ date: dateRegex });
+};
